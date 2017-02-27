83be396989da5bbeb6457129ab607935a88033c4
aymericgoube <aymericgoube@hotmail.fr>
2017-02-27 11:51:13 +0100

    ajout de la premiere section aymeric

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 
 // Création de l'élément title
-let head = document.getElementsByTagName("head")[0]; 
-let titleTag = document.createElement("title"); 
+let head = document.getElementsByTagName("head")[0];
+let titleTag = document.createElement("title");
 let title = document.createTextNode("Exercice JavaScript");
 
 titleTag.appendChild(title);
 head.appendChild(titleTag);
 
 // création du main
-let main = document.createElement('main'); 
-let body = document.getElementsByTagName('body')[0]; 
-body.appendChild(main); 
+let main = document.createElement('main');
+let body = document.getElementsByTagName('body')[0];
+body.appendChild(main);
 
 
 // la partie de Abdoulaye
@@ -32,3 +32,24 @@ let contentParagraph = document.createTextNode("Lorem ipsum dolor sit amet, cons
 un contenu pour le paragraphe */
 paragraph.appendChild(paragraph); // rajoute le contenu au paragraphe
 document.body.appendChild(paragraph); // rajoute le paragraphe et son contenu au body
+
+// Partie de Aymeric
+var section = document.createElement("section");
+// création de la section
+var header = document.createElement("header");
+// création du header
+var headerH2 = document.createElement("h2");
+// création de la balise h2
+var headerH2Text = document.createTextNode("Lorem ipsum");
+// ajout du texte
+headerH2.appendChild(headerH2Text);
+header.appendChild(headerH2);
+
+var paragraphe = document.createElement("p");
+var paragrapheText = document.createTextNode("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.");
+
+paragraphe.appendChild(paragrapheText);
+section.appendChild(header);
+section.appendChild(paragraphe);
+document.body.appendChild(section);
+// Fin de la partie d'Aymeric
